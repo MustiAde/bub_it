817e00faeed00450ed647ea0a10a15c4075b7e59
Edoki Chukwuyem <edokichuks@gmail.com>
2022-06-30 01:50:16 +0100

    Detailed_stats UI Completed

--- lib/screens/detailed_stats.dart
+++ lib/screens/detailed_stats.dart
@@ -23,179 +23,197 @@ class _DetailedStatsState extends State<DetailedStats> {
             body: Stack(
               children: [
                 const Background(),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 25.w),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const AppBalDetail(),
-                      SizedBox(
-                        height: 8.h,
-                      ),
-                      const BackHome(),
-                      SizedBox(height: 30.h),
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 8, horizontal: 8),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(4),
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.shade400,
-                                //  blurRadius: 1,
-                                offset: const Offset(0, 2),
-                              ),
-                            ]),
-                        child: Text(
-                          'Detailed Stats',
-                          style: TextStyle(
-                              color: Colors.grey.shade800,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 23),
+                SingleChildScrollView(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 25.w),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const AppBalDetail(),
+                        SizedBox(
+                          height: 8.h,
                         ),
-                      ),
-                      SizedBox(height: 20.h),
-                      const FaceBookLink(),
-                      SizedBox(height: 20.h),
-                      Text(
-                        'The total number of clicks that your link has received do far:',
-                        style: TextStyle(
-                            color: Colors.grey.shade800, fontSize: 16.sm),
-                      ),
-                      SizedBox(height: 20.h),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            padding: EdgeInsets.symmetric(
-                                vertical: 8.h, horizontal: 8.w),
-                            margin: EdgeInsets.symmetric(
-                                vertical: 4.h, horizontal: 110.w),
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              color: butColor.withOpacity(0.4),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Text(
-                              '88',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.grey.shade800,
-                                  fontSize: 30.sm),
-                            ),
-                          ),
-                          Text(
-                            'Clicks',
+                        const BackHome(),
+                        SizedBox(height: 30.h),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8, horizontal: 8),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(4),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.shade400,
+                                  offset: const Offset(0, 2),
+                                ),
+                              ]),
+                          child: Text(
+                            'Detailed Stats',
                             style: TextStyle(
+                                color: Colors.grey.shade800,
                                 fontWeight: FontWeight.bold,
-                                color: Colors.grey.shade700,
-                                fontSize: 15.sm),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 20.h),
-                      Container(
-                        height: 280.h,
-                        // padding: EdgeInsets.symmetric(
-                        // //     vertical: 15.h, horizontal: 30.w),
-                        // decoration: BoxDecoration(
-                        //   color: Colors.white,
-                        //   borderRadius: BorderRadius.circular(8),
-                        //   border: Border.all(
-                        //     color: Colors.grey.shade300,
-                        //     width: 1,
-                        //   ),
-                        // ),
-                        padding: EdgeInsets.symmetric(
-                            vertical: 8.h, horizontal: 12.w),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            color: Colors.white,
-                            border: Border.all(
-                              color: Colors.grey.shade300,
-                              width: 1.5,
-                            ),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.shade400,
-                                blurRadius: 1,
-                                offset: const Offset(0, 2),
-                              ),
-                            ]),
-                        child: SingleChildScrollView(
-                          child: Column(
-                            //mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              //SizedBox(height: 20.h),
-
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  Text(
-                                    'Location',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.grey.shade800,
-                                        fontSize: 18.sm),
-                                  ),
-                                  Text(
-                                    'Timestamp',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.grey.shade800,
-                                        fontSize: 18.sm),
-                                  ),
-                                ],
-                              ),
-
-                              const LocationTimestamp(
-                                name: 'Edoki Chuks',
-                                time: '2',
-                              ),
-                              const LocationTimestamp(
-                                name: 'Edoki Chuks',
-                                time: '2',
-                              ),
-                              const LocationTimestamp(
-                                name: 'Edoki Chuks',
-                                time: '2',
-                              ),
-                              const LocationTimestamp(
-                                name: 'Edoki Chuks',
-                                time: '2',
-                              ),
-                              const LocationTimestamp(
-                                name: 'Edoki Chuks',
-                                time: '2',
-                              ),
-                              const LocationTimestamp(
-                                name: 'Edoki Chuks',
-                                time: '2',
-                              ),
-                              const LocationTimestamp(
-                                name: 'Edoki Chuks',
-                                time: '2',
-                              ),
-                              const LocationTimestamp(
-                                name: 'Edoki Chuks',
-                                time: '2',
-                              ),
-                              SizedBox(
-                                height: 20.h,
-                              ),
-                            ],
+                                fontSize: 23),
                           ),
                         ),
-                      ),
-                    ],
+                        SizedBox(height: 20.h),
+                        const FaceBookLink(),
+                        SizedBox(height: 20.h),
+                        Text(
+                          'The total number of clicks that your link has received do far:',
+                          style: TextStyle(
+                              color: Colors.grey.shade800, fontSize: 16.sm),
+                        ),
+                        SizedBox(height: 20.h),
+                        const NumbersOfClicks(),
+                        SizedBox(height: 20.h),
+                        Container(
+                          height: 280.h,
+                          padding: EdgeInsets.symmetric(
+                              vertical: 8.h, horizontal: 12.w),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: Colors.white,
+                              border: Border.all(
+                                color: Colors.grey.shade300,
+                                width: 1.5,
+                              ),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.shade400,
+                                  blurRadius: 1,
+                                  offset: const Offset(0, 2),
+                                ),
+                              ]),
+                          child: SingleChildScrollView(
+                            child: Table(
+                              columnWidths: const {0: FlexColumnWidth(0.4)},
+                              defaultVerticalAlignment:
+                                  TableCellVerticalAlignment.middle,
+                              children: [
+                                TableRow(
+                                  children: [
+                                    Text(
+                                      '',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.grey.shade800,
+                                          fontSize: 18.sm),
+                                    ),
+                                    Text(
+                                      'Location',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.grey.shade800,
+                                          fontSize: 18.sm),
+                                    ),
+                                    Text(
+                                      'Timestamp',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.grey.shade800,
+                                          fontSize: 18.sm),
+                                    ),
+                                  ],
+                                ),
+                                namesList(name: 'Edoki Chuks', time: '1'),
+                                namesList(name: 'Musti Adekule', time: '4'),
+                                namesList(name: 'Mj Don', time: '3'),
+                                namesList(name: 'Praise Johnson', time: '1'),
+                                namesList(
+                                    name: 'A very long name Chuks', time: '10'),
+                                namesList(name: 'Isaac Frances', time: '3'),
+                                namesList(name: 'Mercy Garfield', time: '9'),
+                                namesList(name: 'Edoki Chuks', time: '2'),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 )
               ],
             ),
           );
         }));
+  }
+
+  TableRow namesList({required String name, required String time}) {
+    return TableRow(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade600,
+              offset: const Offset(0, 2),
+            ),
+          ]),
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: Icon(
+            Icons.person_outline,
+            color: Colors.grey.shade800,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: Text(
+            name,
+            style: TextStyle(color: Colors.grey.shade800, fontSize: 16.sm),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: Text(
+            '$time hours ago',
+            style: TextStyle(
+                //     fontWeight: FontWeight.bold,
+                color: Colors.grey.shade800,
+                fontSize: 18.sm),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class NumbersOfClicks extends StatelessWidget {
+  const NumbersOfClicks({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          padding: EdgeInsets.symmetric(vertical: 8.h, horizontal: 8.w),
+          margin: EdgeInsets.symmetric(vertical: 4.h, horizontal: 110.w),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: butColor.withOpacity(0.4),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Text(
+            '88',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.grey.shade800,
+                fontSize: 30.sm),
+          ),
+        ),
+        Text(
+          'Clicks',
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.grey.shade700,
+              fontSize: 15.sm),
+        ),
+      ],
+    );
   }
 }
 
@@ -273,79 +291,6 @@ class BackHome extends StatelessWidget {
             'Back to My URLs',
             style: TextStyle(
                 color: butColor, fontWeight: FontWeight.bold, fontSize: 14.sm),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class LocationTimestamp extends StatelessWidget {
-  final String name;
-  final String time;
-  const LocationTimestamp({
-    Key? key,
-    required this.name,
-    required this.time,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 9, horizontal: 10),
-      margin: EdgeInsets.symmetric(
-        vertical: 6.h,
-      ),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(4),
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.shade400,
-              //  blurRadius: 1,
-              offset: const Offset(0, 1),
-            ),
-          ]),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Icon(
-                Icons.person_outline,
-                color: Colors.grey.shade800,
-              ),
-              SizedBox(
-                width: 5.w,
-              ),
-              Text(
-                name,
-                maxLines: 2,
-                softWrap: true,
-                textDirection: TextDirection.rtl,
-                style: TextStyle(
-
-                    //  fontWeight: FontWeight.bold,
-                    color: Colors.grey.shade800,
-                    fontSize: 18.sm),
-              ),
-              SizedBox(
-                width: 25.w,
-              ),
-            ],
-          ),
-
-          // Icon(Icons.person_outline),
-          Text(
-            '$time hours ago',
-            style: TextStyle(
-                //     fontWeight: FontWeight.bold,
-                color: Colors.grey.shade800,
-                fontSize: 18.sm),
-          ),
-          SizedBox(
-            width: 1.w,
           ),
         ],
       ),
